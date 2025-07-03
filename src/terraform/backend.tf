f4d7fc22a639f327e0179e3374c44bf1bb7f575e
brunocarmozini <brunocarmozini21@gmail.com>
2025-07-03 11:27:58 -0300

    md

--- src/terraform/backend.tf
+++ src/terraform/backend.tf
@@ -1,9 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "meu-bucket-terraform-estado"
-    key    = "meu-projeto/terraform.tfstate"
+    bucket = "libertas-bruno-s3"
+    key    = "meu-projeto/terraform.tfstate"  # pode mudar para o caminho que quiser no bucket
     region = "us-east-1"
   }
 }
-
-# 2
